fa98e2fdaa904b21f50dcf9163b6d13584d42ddc
Abisatria <abi.kadek@gmail.com>
2021-12-26 22:33:59 +0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -5,9 +5,8 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-df
+select_box = st.selectbox('Coba selectbox', [1,2,3,4,5])  
+
+if st.button('Recommend') : 
+  print('mantap')
